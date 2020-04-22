aadbe6b8d350519100c66ba62479b882cb92a14f
Adarsh Rao <42054041+adarshr10@users.noreply.github.com>
2020-04-22 18:09:21 -0400

    Update GameObj.java

--- GameObj.java
+++ GameObj.java
@@ -1,9 +1,3 @@
-/**
- * CIS 120 Game HW
- * (c) University of Pennsylvania
- * @version 2.1, Apr 2017
- */
-
 import java.awt.*;
 import java.util.Collections;
 
@@ -296,16 +290,6 @@ public abstract class GameObj {
     		return null;
     	}
     }
-
-    /**
-     * Default draw method that provides how the object should be drawn in the GUI. This method does
-     * not draw anything. Subclass should override this method based on how their object should
-     * appear.
-     * 
-     * @param g The <code>Graphics</code> context used for drawing the object. Remember graphics
-     * contexts that we used in OCaml, it gives the context in which the object should be drawn (a
-     * canvas, a frame, etc.)
-     */
     public abstract void draw(Graphics g);
 
 
