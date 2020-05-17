9e030e66549290af6783a7558b50abb6a11eb099
Adarsh Rao <42054041+adarshr10@users.noreply.github.com>
2020-05-17 14:39:10 -0400

    Update Direction.java

--- Direction.java
+++ Direction.java
@@ -1,10 +1,4 @@
 /**
- * CIS 120 Game HW
- * (c) University of Pennsylvania
- * @version 2.0, Mar 2013
- */
-
-/**
  * This file holds an enumeration called Direction, which is used in GameObj.java to indicate the
  * direction an object should move after it collides with another object.
  * 
